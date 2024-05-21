1950350469972541824ea4c3017f23c6039d8efe
churchrat <mihailkrustev08@gmail.com>
2024-05-21 22:28:31 +0300

    com1

---
+++ Minerbook/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Minerbook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> minerbook = new Dictionary<string, int>();
+            string input = null;
+            int quantity = 0;
+            do
+            {
+                input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    break;
+                }
+                quantity = int.Parse(Console.ReadLine());
+                minerbook.Add(input, quantity);
+
+            } while (input != "stop");
+            foreach (var pair in minerbook)
+            {
+                Console.WriteLine($"{pair.Key} ======> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ Phonebook/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Phonebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+            string input = null;
+            do
+            {
+                input = Console.ReadLine();
+                string[] temp = (input.Split(' '));
+                if (temp[0] == "A")
+                {
+                    if (phonebook.ContainsKey(temp[1]))
+                    {
+                        phonebook.Remove(temp[1]);
+                    }
+                    phonebook.Add(temp[1], temp[2]);
+                }
+                else if (temp[0] == "S")
+                {
+                    bool check = phonebook.ContainsKey(temp[1]);
+                    if (check)
+                    {
+                        Console.WriteLine(temp[1]);
+                        Console.WriteLine(phonebook[temp[1]]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {temp[1]} does not exist. ");
+                    }
+                }
+            } while (input != "END");
+        }
+    }
+}
+
